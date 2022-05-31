fd3870d0de290a26101bf9a64b9fcdc997d0e0b0
Vadym <vadimposhuk1999@gmail.com>
2022-05-31 16:47:12 +0300

    finished project

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,7 +4,8 @@ const modals = () => {
       const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
             
       trigger.forEach(item => {
          item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
             
             modal.style.display = 'block';
             document.body.style.overflow = 'hidden';
+            document.body.style.marginRight = `${scroll}px`;
          });
       });
 
@@ -28,6 +30,7 @@ const modals = () => {
             });
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = '0px';
          }
       });
       
@@ -37,6 +40,7 @@ const modals = () => {
          });
          modal.style.display = 'none';
          document.body.style.overflow = '';
+         document.body.style.marginRight = '0px';
       });
    };
 
@@ -46,6 +50,22 @@ const modals = () => {
          document.body.style.overflow = 'hidden';
       }, time);
    }
+
+   function calcScroll(){
+      const div = document.createElement('div');
+      
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.overflowY = 'scroll';
+      div.style.visibility = 'hidden';
+      document.body.appendChild(div);
+
+      let scrollWidth = div.offsetWidth - div.clientWidth;
+
+      div.remove();
+      return scrollWidth;
+   }
+   calcScroll();
 
    bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
    bindModals('.phone_link', '.popup', '.popup .popup_close');
